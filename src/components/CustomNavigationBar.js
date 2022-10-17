fa36bbb644a69e33118b2b05881d3891eae30913
Leandro <atiladevelop@gmail.com>
2022-10-17 13:08:33 -0300

    Drawer button toggle added

--- src/components/CustomNavigationBar.js
+++ src/components/CustomNavigationBar.js
@@ -1,10 +1,14 @@
 import {Appbar} from 'react-native-paper';
 
-export default function CustomNavigationBar({options}) {
-  console.log({options});
+export default function CustomNavigationBar({navigation, options}) {
   return (
     <Appbar.Header>
       <Appbar.Content title={options.title} />
+      <Appbar.Action
+        accessibilityLabel="Menu"
+        icon="menu"
+        onPress={navigation.toggleDrawer}
+      />
     </Appbar.Header>
   );
 }
