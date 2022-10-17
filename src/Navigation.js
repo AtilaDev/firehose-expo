feb93fdf68e0966b4efca7ab1ba6d86a015dd874
Leandro <atiladevelop@gmail.com>
2022-10-17 13:11:48 -0300

    Wrap Another screen in stack

--- src/Navigation.js
+++ src/Navigation.js
@@ -32,7 +32,22 @@ function HelloDetail() {
   );
 }
 
-function Another() {
+const AnotherStack = createNativeStackNavigator();
+const Another = () => (
+  <AnotherStack.Navigator
+    screenOptions={{
+      header: props => <CustomNavigationBar {...props} />,
+    }}
+  >
+    <AnotherStack.Screen
+      name="AnotherDetail"
+      component={AnotherDetail}
+      options={{title: 'Another!!!'}}
+    />
+  </AnotherStack.Navigator>
+);
+
+function AnotherDetail() {
   return <Text>Another Screen</Text>;
 }
 
